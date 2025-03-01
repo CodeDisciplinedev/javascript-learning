3455ff7a8199f06b4f411dcf972cad6f9bcfd78c
CodeDisciplinedev <tshoaib381@gmail.com>
2025-03-01 10:13:52 +0500

    First commit

--- index.js
+++ index.js
@@ -1,123 +1,117 @@
-// let something = (callback) => {
-//   console.log("something")
-//   callback()
-// }
+// Example of Callback Function
+let something = (callback) => {
+  console.log("Executing something...");
+  callback();
+};
 
-// let something2 = () =>{
-//   console.log("Something 2")
-// }
+let something2 = () => {
+  console.log("Executing something2...");
+};
 
-// something(something2)
+something(something2);
 
+// JavaScript behaves synchronously, meaning it executes code line by line. 
+// If a function takes time (e.g., 1 minute), it blocks the execution of subsequent code.
 
-// javascript scronous ki tarah behave krti ha jase agar mene 2 code likhy wo to forn chl gae lekin jb mene esacode likha jis ko 1 mnt lage ga chlny ma to wo jb tk ni chlta  tb tk us k bd wala code bhi ni chlyga ya  sahi ni ha
+// To avoid this, we use `setTimeout()` because in real-world applications, 
+// we communicate with databases, which takes time to respond.
 
-// is se bachny k liye hum settimeout function us krty ha q k real world project k liye hum database se communicate krta ha agar mera database america ma ha to us ko phly request krni hogi us ma time lagy ga example k liye ye ha to settimeout function hum use kr skty ha  
+console.log("First");
 
-// console.log("first")
+setTimeout(() => {
+  console.log("Executed after 3 seconds");
+}, 3000);
 
-// setTimeout(() =>{
-// console.log("3sec late")
-// },3000)
+console.log("Second");
 
-// console.log("second")
+// Issue with synchronous execution: The following function doesn't return the expected value
+const loginUser = (email, password) => {
+  setTimeout(() => {
+    return email;
+  }, 2000);
+};
 
+const userEmail = loginUser("mastermin22", 2222);
+console.log(userEmail); // This will print `undefined`
 
-// const loginuser = (email , password ) =>{
-//   setTimeout(() => {                       
-//     return(email)
-//   }, 2000);
-// }
-//  jb loginuser wala function chala to ye settimeout wala function to alag stack ma chala gya ab loginuser ma kuch bacha hi ni to ye undefine hogaya ab is k liye hum callback function use kr skty ha
-// const useremail = loginuser("mastermin22", 2222 )
-// console.log(useremail)
+// Solution: Using Callbacks
+const loginUserWithCallback = (email, password, callbackFunc) => {
+  setTimeout(() => {
+    callbackFunc(email);
+  }, 2000);
+};
 
-// const loginuser = (email , password , callbackfunc ) =>{
-//    setTimeout(() => {                       
-//       callbackfunc(email)
-//      }, 2000);
-// }
-// loginuser("mastermin22", 2222 , (email) =>{console.log(email)} )
+loginUserWithCallback("mastermin22", 2222, (email) => {
+  console.log(email);
+});
 
+// Problem with Callbacks: Callback Hell (nested callbacks become hard to read & manage)
+// Solution: Use Promises
 
+const checkMarks = (marks) => {
+  return new Promise((resolve, reject) => {
+    if (marks >= 80) {
+      resolve("Pass");
+    } else {
+      reject("Fail");
+    }
+  });
+};
 
-// ab callback ma bhi issue ha q ye callback hell bn jata ha isko samjhny ma preshani hoti ha aur code bhi zayda likhna prta ha is k liye hum promise use krty ha is ki example kuch esi ha mene ek function banya us ma parameter pass kr diya marks ab hum promise ki madad se isko resolve kr skty ha ya reject mtlb k mene kha k jis k 80 se uper ya 80 marks ha us ko pass kr do baki fail to ma rosolve aur reject laga skta ha wase hum kuch bhi likh skty ha resolve aur reject k ilwa bhi to ma resolve kr dunga jis k 80 huwe aur jis k 80 se km ae wo reject phr hum isko .then aur .catch ki madad se console.log kr k print krwaenge
-// const promiss = (marks) =>{
-//  return new Promise((resolve, reject) => {
-//     if (marks >= 80) {
-//       resolve("pass")
-      
-//     } else {
-//       reject("fail")
-      
-//     }
-//   })
-// }
+checkMarks(80)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-// promiss(80).then((pass)=>{
-//   console.log(pass)
-// }).catch((fail)=>{
-//   console.log(fail)
-// })
+// Using Promises in a Login System
+const loginUserPromise = (email, password) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (email === "tahirshobb@gmail.com" && password === "1234") {
+        resolve(email);
+      } else {
+        reject("Email or password incorrect");
+      }
+    }, 2000);
+  });
+};
 
-// callback ma ek issue tha k wo nested bn rhy thy ab hum ne ek function banaya jis ma hum ny parameter ps kiya email aur password ap promie function ko use kr k hum resolve aur reject krengy mene ek settimeout lagaya k 2 sec bd email print hojae lekin mene condition likhi k jb email ps ki jae to ___ email ho aur password ___ agr email aur password match hojae jae to email print kr dena wrna reject phr mene ek aur function banaya jis ma mene kha k agr email correct ati ha to usy video show ho jae lekin isko hendle krny k liye hamare ps 2 funcction hoty ha ek .then aur .catch then kya krta ha k agar ap ny jo ps kiya wo resolve hojata ha to .then ma jo ps kiya  hoga wo print hojaega aur agar apki condition match ni hoti to catch ma bhej dega phr catch print hojaega this is very simple 
-// const loginuser = (email , password ) =>{
-// return new Promise((resolve, reject) => {
-//   setTimeout(() => { 
-//     if (email==="tahirshobb@gmail.com" && password==="1234") {
-//       resolve(email)
-//     }else{
-//       reject("email and password incoorect")
-//     }
-//   }, 2000);
-// })
-//    }
+const getVideos = (email) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(["video1", "video2", "video3"]);
+    }, 2000);
+  });
+};
 
-// const getvideo =(email) =>{
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve(["video1","viddeo2","video3"])
-//     }, 2000);
-//   })
-// }
+loginUserPromise("tahirshobb@gmail.com", "1234")
+  .then((email) => {
+    console.log(email);
+    return getVideos(email);
+  })
+  .then((videos) => {
+    console.log(videos);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-// loginuser("tahirshobbb@gmail.com","1234")
-// .then((e)=>{
-//   console.log(e)
-//   return getvideo(e)
-// }).then((video)=>{console.log(video)})
-// .catch((err)=>{
-//   console.log(err)
-//  })
+// Problem with .then & .catch: It becomes repetitive and hard to maintain
+// Solution: Use Async/Await
 
-// ab .then aur .catch ma bhi ek issue q k jb hum .then aur .catch use krty ha to ye humen br br use krna prega ise code ko samjhne ma muskil q k itne then aur catch hongy  masla hoga ab is k liye hum async await ka use krty ha lekin hum ny jo promise abhi kya tha wo to background ma run krega lekin .then aur .catch ko hum use ni krengy is k liye hum async await use krengy ab mene async function banaya ab us ma useremail bnaya aur await kr diya ab await hum ny is liye lagaya k hum ny jo promise diya ha wo resolve ni hojata tb tk wait kro ab agr ma await ni lgata to ye status pending show krega q k wo wahan py ruka hi ni agae nikl gaya is liye humny await lagaya ab isko hendle krny k liye mere ps try aur catch hoty ha try ye krta ha k ma try krta hu agar value resolve ajati ha to ma print hojaunga wrna ma catch ma bhej dunga aur catch ma hum error bhej dengy simple lekin mujhy isko call krna prega jo mene getuserdetail function banaya ha usko
-// const loginuser = (email , password ) =>{
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => { 
-//       if (email==="tahirshobb@gmail.com" && password==="1234") {
-//         resolve(email)
-//       }else{
-//         reject("email and password incoorect")
-//       }
-//     }, 2000);
-//   })
-//      }
-  
-//   const getvideo =(email) =>{
-//     return new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//         resolve(["video1","viddeo2","video3"])
-//       }, 2000);
-//     })
-//   }
-// const getuserdetail = async () => {
-// try {
-//   const useremail = await loginuser("tahirshobb@gmail.com","1234")
-//   console.log(useremail)
-//   const uservideo = await getvideo(useremail)
-//   console.log(uservideo)
-// } catch (error) {
-//   console.log("error")
-// }
-// }
-// getuserdetail()
+const getUserDetails = async () => {
+  try {
+    const userEmail = await loginUserPromise("tahirshobb@gmail.com", "1234");
+    console.log(userEmail);
+
+    const userVideos = await getVideos(userEmail);
+    console.log(userVideos);
+  } catch (error) {
+    console.log("Error:", error);
+  }
+};
+
+getUserDetails();
